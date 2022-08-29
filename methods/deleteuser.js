92a430db2cc4159f48ec4bf818d8d70d3357a45a
70579651+TheCreeperZenior@users.noreply.github.com <70579651+TheCreeperZenior@users.noreply.github.com>
2022-08-28 21:43:23 -0400

    fixed a crash

--- methods/deleteuser.js
+++ methods/deleteuser.js
@@ -8,19 +8,26 @@ const getUser = new express.Router();
 getUser.post("/deleteuser", async (req,res) =>{
     const data = req.body;
     var confirmation = ""
-
-    if (data.rol == "*")
+    try {
+        if (data.rol == "*")
+        {
+            return res.status(200).json({
+                status: false
+            });
+        } else {
+            confirmation = await usuarios.deleteOne({_id: data._id});
+            console.log(confirmation)
+            return res.status(200).json({
+                status: true
+            });
+        }
+    } catch (e)
     {
         return res.status(200).json({
             status: false
         });
-    } else {
-        confirmation = await usuarios.deleteOne({_id: data._id});
-        console.log(confirmation)
-        return res.status(200).json({
-            status: true
-        });
     }
+    
 })
 
 module.exports = getUser;
