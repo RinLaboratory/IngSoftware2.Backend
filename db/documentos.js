7daea07a4e2601f660c9f085b78fe56c0d751fdd
Cris-Daiki <cristobaldonosonuevo@gmail.com>
2022-09-26 15:32:35 -0300

    estructura documentos

--- db/documentos.js
+++ db/documentos.js
@@ -4,7 +4,31 @@ const userSchema = new mongoose.Schema({
     name:String,
     lastname:String,
     email:String,
-    password:String
+    phone:String,
+    inscr_Date:String,
+    Bautismo:{
+        padrino: String,
+        madrina: String,
+        fecha: String,
+        padre: String,
+        lugar: String
+    },
+    Confirmacion:{
+        padrino: String,
+        madrina: String,
+        fecha: String,
+        padre: String,
+        lugar: String
+    },
+    Matrimonio: {
+        padrino: String,
+        madrina: String,
+        pareja: String,
+        fecha: String,
+        padre: String,
+        lugar: String,
+    },
+    Referencia:String,
 })
 
 module.exports = mongoose.model("documentos", userSchema);
