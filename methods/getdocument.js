1fdfeabbfbec42a920ee19bd72885e8eed2034cf
cristobal_D <cristobaldonosonuevo@gmail.com>
2022-09-26 19:13:54 -0300

    arreglos en getdocument funcionalidad botton

--- methods/getdocument.js
+++ methods/getdocument.js
@@ -9,12 +9,32 @@ const getdocument = new express.Router();
 getdocument.post("/getdocument", async (req,res) =>{
     const data = req.body
     const texto = req.body.texto
-    console.log(texto)
+    const varDocumentoBautismo = []
+    const varDocumentoMatrimonio = []
+    const varDocumentoConfirmacion = []
     let nameRegex = new RegExp(texto);
     let doc = await documentos.find({name: {$regex: nameRegex, $options: 'i'}})
-    let Bautismo = await documentos.find({Bautismo: {$regex: nameRegex}})
-    console.log(doc)
-
+    let Bautismo = await documentos.find({name: {$regex: nameRegex, $options: 'i'}})
+    let Confirmacion = await documentos.find({name: {$regex: nameRegex, $options: 'i'}})
+    let Matrimonio = await documentos.find({name: {$regex: nameRegex, $options: 'i'}})
+    for (var x = 0; x < Bautismo.length; x++ ){
+        if(Bautismo[x].Bautismo.fecha != ""){
+            varDocumentoBautismo.push(Bautismo[x]) 
+        }
+        
+    }
+    for (var x = 0; x < Matrimonio.length; x++ ){
+        if(Matrimonio[x].Matrimonio.fecha != ""){
+            varDocumentoMatrimonio.push(Matrimonio[x]) 
+        }
+        
+    }
+    for (var x = 0; x < Confirmacion.length; x++ ){
+        if(Confirmacion[x].Confirmacion.fecha != ""){
+            varDocumentoConfirmacion.push(Confirmacion[x]) 
+        }
+        
+    }
     var documents = "";
     
         if (data.search == "NOMBRE") {
@@ -40,7 +60,10 @@ getdocument.post("/getdocument", async (req,res) =>{
     return res.status(200).json({
         documents: documents,
         doc: doc,
-        Bautismo: Bautismo
+        Bautismo: varDocumentoBautismo,
+        Matrimonio: varDocumentoMatrimonio,
+        Confirmacion: varDocumentoConfirmacion
+        
     });
     
 })
