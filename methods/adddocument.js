c76041e69bec2629cf75c42fe36eb96e12b458d1
Cris-Daiki <cristobaldonosonuevo@gmail.com>
2022-09-26 18:18:31 -0300

    arreglado error

--- methods/adddocument.js
+++ methods/adddocument.js
@@ -8,11 +8,30 @@ const adddocument = new express.Router();
 
 adddocument.post("/adddocument", async (req,res) =>{
     const data = req.body
-    console.log(data)
-    let documento = new documentos(data)
-    await documento.save() 
-    return res.status(200).json({
-        status: true
-    });
+    var docs = "";
+
+    if (data.name.length !== 0 && data.name.length !== 0)
+    {
+        docs = await documentos.find({name: data.name, lastname: data.lastname, inscr_Date: data.inscr_Date});
+        if (docs.length > 0)
+        {
+            return res.status(200).json({
+                status: false,
+                msg: "document already exists"
+            });
+        } else {
+            let documento = new documentos(data)
+            await documento.save() 
+            return res.status(200).json({
+                status: true
+            });
+        }
+    } else {
+        return res.status(200).json({
+            status: false,
+            msg: "name and lastname cant be blank"
+        });
+    }
+    
 })
 module.exports = adddocument;
