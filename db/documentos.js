65186405a6ca317a84ea71b70d9c9a2f6a0a50da
Cris-Daiki <cristobaldonosonuevo@gmail.com>
2022-09-29 02:12:14 -0300

    actualizar formato documentos

--- db/documentos.js
+++ db/documentos.js
@@ -1,17 +1,24 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+    n_id: String,
+    rut: String,
     name:String,
     lastname:String,
+    father: String,
+    mother: String,
+    birth: String,
     email:String,
     phone:String,
+    Obs: String,
     inscr_Date:String,
     Bautismo:{
         padrino: String,
         madrina: String,
         fecha: String,
         padre: String,
-        lugar: String
+        lugar: String,
+        preparacion: String
     },
     Confirmacion:{
         padrino: String,
