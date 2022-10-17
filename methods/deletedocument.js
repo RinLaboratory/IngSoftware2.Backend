26ff3f252c97ca7e62533e151036992e3fc7054b
TheCreeperZenior <70579651+TheCreeperZenior@users.noreply.github.com>
2022-10-17 01:05:13 -0300

    fixed delete document method

--- methods/deletedocument.js
+++ methods/deletedocument.js
@@ -2,28 +2,36 @@ const express = require("express");
 
 require("../db/config");
 const documento = require("../db/documentos")
+const parents = require("../db/parents")
+const bautismo = require("../db/bautismo")
+const confirmacion = require("../db/confirmacion")
+const matrimonio = require("../db/matrimonio")
 
-const BorrorarDocumento = new express.Router();
+const BorrarDocumento = new express.Router();
 
-BorrorarDocumento.post("/deletdocument", async (req,res) =>{
+BorrarDocumento.post("/deletedocument", async (req,res) =>{
     const data = req.body;
-    var confirmation = ""
+    console.log(data)
     try {
-        if (data.rol == "*")
-        {
-            confirmation = await documento.deleteOne({_id: data._id});
-            return res.status(200).json({
-                status: true
-            });
-        } else {
-            confirmation = await documento.deleteOne({_id: data._id});
-            return res.status(200).json({
-                status: true
-            })
-            
+        if(data.parent_Data.p_id != ""){
+            await parents.deleteOne({_id: data.parent_Data.p_id});
         }
+        if(data.Bautismo.b_id != ""){
+            await bautismo.deleteOne({_id: data.Bautismo.b_id});
+        }
+        if(data.Confirmacion.c_id != ""){
+            await confirmacion.deleteOne({_id: data.Confirmacion.c_id});
+        }
+        if(data.Matrimonio.m_id != ""){
+            await matrimonio.deleteOne({_id: data.Matrimonio.m_id});
+        }
+        await documento.deleteOne({_id: data._id});
+        return res.status(200).json({
+            status: true
+        })
     } catch (e)
     {
+        console.log(e)
         return res.status(200).json({
             status: false
         });
@@ -31,4 +39,4 @@ BorrorarDocumento.post("/deletdocument", async (req,res) =>{
     
 })
 
-module.exports = BorrorarDocumento;
+module.exports = BorrarDocumento;
